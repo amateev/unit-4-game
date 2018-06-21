7b440c4743493911f9b293154a2d671e2a072dce
Alexandra M <alexandramatveyeva@yahoo.com>
2018-06-21 12:46:09 -0700

    js function and on click events for each stone

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,26 +1,104 @@
 var wins = 0;
 var losses = 0;
-var points = 0;
+var pointsSum = 0;
 var randomNumber;
+
 var rubyHiddenNumber;
 var sapphireHiddenNumber;
 var diamondHiddenNumber;
+var emeraldHiddenNumber;
 
-// game starts:
-// new random number is generated
-randomNumber = Math.floor((Math.random() * 102) + 19);
 
-// new random hidden number is generated for:
-// button 1
-rubyHiddenNumber =  Math.floor((Math.random() * 12) + 1);
+function () {
+	// new random number is generated
+	randomNumber = Math.floor((Math.random() * 102) + 19);
+	$('.random-number').html(randomNumber);
 
-// button 2
-sapphireHiddenNumber =  Math.floor((Math.random() * 12) + 1);
-// button 3
-diamondHiddenNumber =  Math.floor((Math.random() * 12) + 1);
-// button 4
-emeraldHiddenNumber =  Math.floor((Math.random() * 12) + 1);
-// points summary = 0
-// if clicking any button a random hidden number is added to the points summary
-// if random number = points summary, win +, alert 'you win' start, new game 
-// if random number > points summary, losses +, alert 'you lost', start new game
+
+	// // new random hidden number is generated for:
+	
+	// rubyHiddenNumber =  Math.floor((Math.random() * 12) + 1);
+	// sapphireHiddenNumber =  Math.floor((Math.random() * 12) + 1);
+	// diamondHiddenNumber =  Math.floor((Math.random() * 12) + 1);
+	// emeraldHiddenNumber =  Math.floor((Math.random() * 12) + 1);
+
+	// if random number equals to sum of points: 
+	// wins increases by 1
+	// wins displays in span wins
+	// string 'You win!' displays in p alert
+	 if (randomNumber == pointsSum) {
+	 	wins++
+	 	$('#wins').html(wins);
+	 	$('#alert').html('You win!');
+	 }
+
+	 // if random number more than pointsSum:
+	 // losses increases by 1
+	// losses displays in span losses
+	// string 'You lost!' displays in p alert
+
+	  if (randomNumber > pointsSum) {
+	 	losses++
+	 	$('#losses').html(losses);
+	 	$('#alert').html('You lost!');
+	 }
+
+}
+
+//by clicking on ruby button
+$('#ruby').on ('click', function () {
+		//random number associated with this button is generated
+		rubyHiddenNumber =  Math.floor((Math.random() * 12) + 1);
+
+		// pointsSum increases by the random number associated with ruby
+		pointsSum = pointsSum + rubyHiddenNumber;
+
+		//number id displayed in points-sum div
+		$('#points-sum').html(pointsSum);
+	})
+
+//by clicking on ruby button
+$('#sapphire').on ('click', function () {
+		//random number associated with this button is generated
+		sapphireHiddenNumber =  Math.floor((Math.random() * 12) + 1);
+
+		// pointsSum increases by the random number associated with sapphire
+		pointsSum = pointsSum + sapphireHiddenNumber;
+
+		//number id displayed in points-sum div
+		$('#points-sum').html(pointsSum);
+	})
+
+//by clicking on ruby button
+$('#diamond').on ('click', function () {
+		//random number associated with this button is generated
+		diamondHiddenNumber =  Math.floor((Math.random() * 12) + 1);
+
+		// pointsSum increases by the random number associated with diamond
+		pointsSum = pointsSum + diamondHiddenNumber;
+
+		//number id displayed in points-sum div
+		$('#points-sum').html(pointsSum);
+	})
+
+//by clicking on ruby button
+$('#emerald').on ('click', function () {
+		//random number associated with this button is generated
+		emeraldHiddenNumber =  Math.floor((Math.random() * 12) + 1);
+
+		// pointsSum increases by the random number associated with emerald
+		pointsSum = pointsSum + emeraldHiddenNumber;
+
+		//number id displayed in points-sum div
+		$('#points-sum').html(pointsSum);
+	})
+
+
+
+
+
+	
+
+
+
+
